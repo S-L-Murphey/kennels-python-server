10521fba0c5728b6f73d2a76d76bf7474db71a5c
S-L-Murphey <smurphey1990@gmail.com>
2021-07-12 15:24:49 -0500

    Added Models Packages and classes for all modules

---
+++ models/customer.py
@@ -0,0 +1,7 @@
+class Customer():
+
+    def __init__(self, id, name, location_id):
+        self.id = id
+        self.name = name
+        self.location_id = location_id
+        
---
+++ models/employee.py
@@ -0,0 +1,7 @@
+class Employee():
+
+    def __init__(self, id, email, name, address):
+        self.id = id
+        self.email = email
+        self.name = name
+        self.address = address
